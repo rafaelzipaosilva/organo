04a69caf6f4a4986306a8d4a78c70e893555e56c
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-04-10 16:50:00 -0300

    aula3

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,8 +2,15 @@ import './CampoTexto.css'
 
 const CampoTexto = ({ label, placeholder, valor, aoAlterado, obrigatorio = false }) => {
     return (<div className='campo-texto'>
-        <label>{label}</label>
-        <input value={valor} onChange={evento => aoAlterado(evento.target.value)} required={obrigatorio} placeholder={placeholder}/> 
+        <label>
+            {label}
+        </label>
+        <input 
+            value={valor}
+            onChange={evento => aoAlterado(evento.target.value)}
+            required={obrigatorio}
+            placeholder={placeholder}
+        /> 
     </div>)
 }
 
